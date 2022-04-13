31cc2358e42ace9ffa03a062a48391d79cb47da0
DylanTsai1 <101568962+DylanTsai1@users.noreply.github.com>
2022-04-13 10:30:24 -0300

    Tema 1 Simulacro Terminado

---
+++ Assets/test1.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test1 : MonoBehaviour
+{
+    public float pesos;
+    public string monedaextranjera;
+    float convertir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (pesos>=1000)
+        {
+            if (monedaextranjera=="D")
+            {
+                convertir = pesos/200;
+                Debug.Log("El monto que ha ingresado es convertido en " + convertir + " dólares");
+            }
+            else if (monedaextranjera == "R")
+            {
+                convertir = pesos/25;
+                Debug.Log("El monto que ha ingresado es convertido en " + convertir + " reales");
+            }
+            else if (monedaextranjera == "E")
+            {
+                convertir = pesos / 220;
+                Debug.Log("El monto que ha ingresado es convertido en " + convertir + " euros");
+            }
+            else
+            {
+                Debug.Log("Opción de moneda extranjera no válida");
+            }
+        }
+
+        else
+        {
+            Debug.Log("El monton minimo es de $1000");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
